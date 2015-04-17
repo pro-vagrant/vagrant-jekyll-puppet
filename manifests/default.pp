777302c31bb67f80bd2336d310b000d6c88172f2
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-04-17 13:08:20 +0200

    Use ruby-1.9.1-dev instead of RVM

--- manifests/default.pp
+++ manifests/default.pp
@@ -10,18 +10,11 @@ package { 'lynx-cur':
     require => Exec['apt-get update']
 }
 
-exec { 'GPG key':
-    command => '/usr/bin/gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3',
+package { 'ruby1.9.1-dev':
     require => Exec['apt-get update']
 }
 
-exec { 'Install RVM with Ruby':
-    command => '/usr/bin/curl -sSL https://get.rvm.io | /bin/bash -s stable --ruby',
-    timeout => 6000,
-    require => Exec['GPG key']
-}
-
 exec { 'Install Jekyll':
-    command => '/bin/bash --login -c "gem install jekyll"',
+    command => 'gem install jekyll',
     require => Exec['Install RVM with Ruby']
 }
